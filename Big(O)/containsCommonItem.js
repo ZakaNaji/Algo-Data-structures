f61d524c3cb86980ed2019263c436281bc405a13
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-07-03 19:58:52 +0100

    Big-O: we did improve containCommonItems func time complexity from a*b => a+b, by introducing a map of the 1st array, but we endup creating a space complexity of o(n); Ps: usually there is a tradoff btw space-time complexity.

--- Big(O)/containsCommonItem.js
+++ Big(O)/containsCommonItem.js
@@ -1,14 +1,21 @@
 function containsCommonItems(arr1, arr2) {
+  //lets 1st create a map of the first array
+  let map = {};
   for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        return true;
-      }
+    if (!map[arr1[i]]) {
+      const item = arr1[i];
+      map[item] = true;
+    }
+  }
+  // lets loop through the second array and check if the item is in the map
+  for (let j = 0; j < arr2.length; j++) {
+    if (map[arr2[j]]) {
+      return true;
     }
   }
   return false;
 }
-containsCommonItems(["a", "b", "c", "x"], ["z", "y", "i"]); // false
-containsCommonItems(["a", "b", "c", "x"], ["z", "y", "x"]); // true
-// Time complexity: O(a * b) because we have two nested loops
-// Space complexity: O(1) because we are not creating any new data structures
+console.log(containsCommonItems(["a", "b", "c", "x"], ["z", "y", "i"])); // false
+console.log(containsCommonItems(["a", "b", "c", "x"], ["z", "y", "x"])); // true
+// Time complexity: O(n + m) because we are looping through both arrays separately
+// Space complexity: O(n) because we are creating a map of the first array
