773d4739b89b8a2dca104ec8fdf6588fb30bfd2a
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-07-06 13:51:06 +0100

    Data structures: 1 Arrays - improve find char occurences function.

--- data_structures/countCharOccurence.js
+++ data_structures/countCharOccurence.js
@@ -1,15 +1,20 @@
 function countCharOccurence(str) {
   const result = {};
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i].toLowerCase();
-    if (char === " ") continue;
-    if (result[char]) {
-      result[char] += 1;
-    } else {
-      result[char] = 1;
-    }
+  for (let char of str) {
+    char = char.toLowerCase();
+    if (!isAlphaNumeric(char)) continue;
+    result[char] = ++result[char] || 1;
   }
   return result;
 }
 
-console.log(countCharOccurence("hello world")); //{ h: 1, e: 1, l: 3, o: 2, w: 1, r: 1, d: 1 }
+function isAlphaNumeric(char) {
+  const charCode = char.charCodeAt(0);
+  return (
+    (charCode > 47 && charCode < 58) ||
+    (charCode > 64 && charCode < 91) ||
+    (charCode > 96 && charCode < 123)
+  );
+}
+
+console.log(countCharOccurence("hello world!")); //{ h: 1, e: 1, l: 3, o: 2, w: 1, r: 1, d: 1 }
