5629d18dbbc7114e4a6abb81ab5d8a6c39eb5976
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-07-17 12:49:34 +0100

    Data Structure: Hash Table - add collision management to our custom implementation.

--- data_structures/hash_table/MyHashTable.js
+++ data_structures/hash_table/MyHashTable.js
@@ -11,15 +11,28 @@ class MyHashTable {
     return hash;
   }
   set(key, value) {
-    this.data[this._hash(key)] = value;
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
   }
   get(key) {
-    return this.data[this._hash(key)];
+    let address = this._hash(key);
+    let currentBucket = this.data[address];
+    if (!currentBucket) return undefined;
+    for (let i = 0; i < currentBucket.length; i++) {
+      if (currentBucket[i][0] === key) {
+        return currentBucket[i][1];
+      }
+    }
   }
 }
 
 const ht = new MyHashTable(50);
 ht.set("grapes", 10000);
 ht.set("scream", "ahhhhhhhhh!");
+ht.set("test", "this is a simple test");
 console.log(ht.get("grapes"));
 console.log(ht.get("scream"));
+console.log(ht.get("test"));
