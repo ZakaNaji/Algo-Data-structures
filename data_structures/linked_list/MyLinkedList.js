01c79038325b6a48df9843b70bbf628ef3a8bf3f
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-07-18 12:08:21 +0100

    Data Structures: Linked List - create custom single linked list (const + append + prepend)

---
+++ data_structures/linked_list/MyLinkedList.js
@@ -0,0 +1,34 @@
+class MyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: this.head,
+    };
+    this.head = newNode;
+    this.length++;
+  }
+}
+const ll = new MyLinkedList(10);
+ll.append(11);
+ll.append(12);
+ll.prepend(9);
+console.log(ll);
